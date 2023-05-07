f45bbf802b046acfe925ac3a95ee62e916cca281
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-06 21:52:31 -0700

    Added Global Error handling

--- src/api/express.ts
+++ src/api/express.ts
@@ -1,12 +1,16 @@
-import express, { Application } from 'express';
+import express, { Application, Response as ExResponse, Request as ExRequest, NextFunction } from 'express';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import { RegisterRoutes } from './routes/routes';
+import { ValidateError } from "tsoa";
+
 const app: Application = express();
 
 //Express middleware
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.static("public"));
+
 //Routing information
 app.use(
     "/docs",
@@ -17,8 +21,29 @@ app.use(
         }
     })
 )
-
-app.use(morgan("tiny"));
-app.use(express.static("public"));
+//Sets routes generated from TSOA to be applied to API
 RegisterRoutes(app);
+
+//Adding Global Error handler 
+app.use( function errorHandler(
+    err: unknown,
+    req: ExRequest,
+    res: ExResponse,
+    next: NextFunction
+    ): ExResponse | void {
+        if (err instanceof ValidateError) {
+            console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+            return res.status(422).json({
+                message: "Validation Failed",
+                details: err?.fields,
+            });
+        }
+        if (err instanceof Error) {
+            return res.status(500).json({
+                message: "Internal Server Error",
+            })
+        }
+        next();
+    });
+
 export default app;
