8e7aaea14e3c3151f21b512f180b41957419b777
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-06 22:03:50 -0700

    implemented TSOA, Added example doc on how to use

--- src/api/controllers/health-controller.ts
+++ src/api/controllers/health-controller.ts
@@ -1,30 +1,23 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Path,
-    Post,
-    Query,
-    Route,
-    SuccessResponse,
-  } from "tsoa";
+/**
+ * PURPOSE: Health Endpoint Controller for Project Wayfinder
+ */
+import { Get, Route, SuccessResponse, Tags, Example } from "tsoa";
 
   interface HealthPointInterface {
-    message: string;
+    response: string
   }
   
   @Route("health")
+  @Tags("Health")
   export class HealthClassController {
+    /**
+     * Project Waypoint's health handler to confirm API is online with Openshift
+     * @summary Health endpoint for OpenShift
+     */
+    @Example<string>("Project Waypoint API Online")
     @SuccessResponse(200, "OK")
-    @Get("/")
-    public async healthCheck(): Promise<HealthPointInterface> {
-      try { 
-        return {
-          message: "Express + Typescript API Online"
-        }
-      } catch(ex) {
-        console.error(ex);
-        return {} as HealthPointInterface
-      }
+    @Get()
+    public async healthCheck() { 
+      return "Project Waypoint API Online" 
     }
   }
