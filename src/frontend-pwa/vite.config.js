9ff6efe0f67deda2a01d47c8e259374ff5445b69
TylerMaloney <123214152+TylerMaloney@users.noreply.github.com>
2023-05-10 13:17:10 -0700

    vite shell added and functional

---
+++ src/frontend-pwa/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    })
+  ],
+})
