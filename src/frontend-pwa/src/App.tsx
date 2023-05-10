2327e8e4a0001bde6d0a0ee43a6bae26026bd451
TylerMaloney <123214152+TylerMaloney@users.noreply.github.com>
2023-05-10 14:43:09 -0700

    swapped vite shell from JS to TS

--- src/frontend-pwa/src/App.tsx
+++ src/frontend-pwa/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+  const [count, setCount] = useState(0)
+  const buttonHandler = (): void => {
+    setCount(count => count+1);
+  }
   return (
     <>
       <div>
@@ -18,16 +21,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p> words! </p>
+        <input type={"button"} value={count} onClick={buttonHandler} /> 
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logo
-      </p>
+
     </>
   )
 }
