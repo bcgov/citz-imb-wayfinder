29d164706ce1150c8e891839d90b1f60a1e60195
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-09 20:20:28 -0700

    created first jest test on /health endpoint

---
+++ src/api/tests/health.test.ts
@@ -0,0 +1,18 @@
+const supertest = require('supertest');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const testPoint = `http://${process.env.HOST}:${process.env.PORT}/api`;
+const request = supertest(testPoint);
+
+/**
+ * Initial Test to show API is working as expected
+ */
+
+describe("Verify API is healthy and online", () => {
+    test("/health returns code 200", async () => {
+        const response = await request.get("/health");
+        expect(response.ok).toBe(true);
+        expect(response.status).toBe(200);
+    });
+});
