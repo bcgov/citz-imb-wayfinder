ced54bcbb80b0448555ba87c371674f4a4ccdce2
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-10 21:13:10 -0700

    convert tests to js and added locationsTest

---
+++ src/api/tests/integration/locations.test.js
@@ -0,0 +1,23 @@
+const supertest = require('supertest');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const testPoint = `http://${process.env.API_HOSTNAME}:${process.env.API_PORT}/api`;
+const request = supertest(testPoint);
+
+/**
+ * Testing the get method of the locations endpoint
+ * Verifying:
+ *       -The API Returns an OK 
+ *       -The API Returns a 200
+ *       -The API is returning an Array under the locations key
+ */
+
+describe("Verify API is healthy and online", () => {
+    test("/locations GET fetching all locations", async () => {
+        const response = await request.get("/locations");
+        expect(response.ok).toBe(true);
+        expect(response.status).toBe(200);
+        expect(typeof response.body.locations).toBe(typeof []);
+    });
+});
