ec20e17c6ee2b0f54f6d814321cdfdaa5b48e15d
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-23 15:50:15 -0700

    Header.tsx Updated

--- src/frontend-pwa/src/components/Header/Header.tsx
+++ src/frontend-pwa/src/components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import {
+  HeaderWrapper,
+  Heading,
+  Banner,
+} from './header.styles';
+
 export default function Header() {
   return (
-    <input
-      type="button"
-      value="Hello"
-    />
+    <HeaderWrapper>
+      <Banner>
+        <a href="https://gov.bc.ca">
+          <img src="../../assets/images/logo-banner.svg" alt="Go to the Government of British Columbia website" />
+        </a>
+        <Heading>Wayfinder</Heading>
+      </Banner>
+    </HeaderWrapper>
   );
 }
