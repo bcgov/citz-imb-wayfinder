7f023ffa5a564d9be004c958179b6962f4bfec9c
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-23 15:57:32 -0700

    App.tsx updated to import and render Header component

--- src/frontend-pwa/src/App.tsx
+++ src/frontend-pwa/src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <input
-      type="button"
-      value="Hello"
-    />
+    <Header />
   );
 }
 
