1d77158d10698da38c0cc630c8a7d21f748e57e7
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-23 15:46:28 -0700

    @emotion/styled added to app dependencies and header.styles updated

---
+++ src/frontend-pwa/src/components/Header/header.styles.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export const HeaderWrapper = styled.header`
+    background-color: #036;
+    border-bottom: 2px solid #fcba19;
+    padding: 0 65px;
+    color: #fff;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    height: 65px;
+    top: 0;
+    position: fixed;
+    width: 100%;
+    left: 0;
+`;
+
+export const Heading = styled.h1`
+    font-family: 'BCSans', 'Noto Sans', Verdana, Arial, sans-serif;
+    font-weight: normal;
+    margin: 5px 5px 0 18px;
+`;
+
+export const Banner = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 0 10px 0 0;
+`;
