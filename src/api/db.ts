90f332e27fde8999d2c0af569945edfc1d4671be
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-23 20:12:31 -0700

    updated docker and readme to reflect keys

--- src/api/db.ts
+++ src/api/db.ts
@@ -7,7 +7,7 @@ mongoose.set('strictQuery', true);
 
 // eslint-disable-next-line no-extra-boolean-cast
 const MONGO_HOSTNAME = !!process.env.CONTAINERIZED ? process.env.MONGO_SERVICE : 'localhost';
-mongoose.connect(`mongodb://${process.env.WAYF_MONGO_USERNAME}:${process.env.WAYF_MONGO_PASSWORD}@${MONGO_HOSTNAME}:${process.env.MONGO_PORT}`);
+mongoose.connect(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${MONGO_HOSTNAME}:${process.env.MONGO_PORT}`);
 
 mongoose.connection.on('connected', () => {
   console.info('Successfully connected to Mongoose');
