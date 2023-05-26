6a77eeefd23423e5edaca7f602cae0e65492d07d
Matthew Logan <loganmatthewr@gmail.com>
2023-05-26 15:59:17 -0700

    Linked to an API to get lat long data.
    Added API for lat long

--- src/scraper/web_scraper.py
+++ src/scraper/web_scraper.py
@@ -2,11 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+from dotenv import dotenv_values
+import urllib
+
+CONFIG = dotenv_values(".env")
 
 #Constants for beginning search and filtering initial results
 BASE_URL = "https://www2.gov.bc.ca"
 START_POINT = '/gov/content/governments/organizational-structure/ministries-organizations/ministries/citizens-services/servicebc'
 FILTER = "/gov/content/governments/organizational-structure/ministries-organizations/ministries/citizens-services/servicebc/service-bc-location-"
+RESULTS_FILE="results.json"
 
 #############################################################################
 # @desc : Given a URL, requests the data and returns the soup object
@@ -36,6 +41,14 @@ def getInitialURLs(soup):
   return destinations
 
 #############################################################################
+# @desc: Take current 
+#############################################################################
+def fetchFromAPI(locationString, localeString):
+  queryString = f"{locationString}, {localeString}, ca"
+  response = requests.get(CONFIG['API_URL'] + urllib.parse.quote_plus(queryString))
+  return response.json()
+
+#############################################################################
 # @desc: Given the Address string from previous linting, parses the string to 
 #        be more granular
 # @returns: Dictionary Object of broken down string
@@ -44,26 +57,33 @@ def refineMailingAddress(mailingStr):
   ADDRESS = 0
   LOCALE = 1
   POSTAL_REGEX = r'[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d'
-  mailingAddress = {}
+  street_address = {}
   mailingArr = mailingStr.strip().split(',')
   if len(mailingArr) > 0: #Confirm Address is present
-    mailingAddress["address"] = mailingArr[ADDRESS]
+    street_address["address"] = mailingArr[ADDRESS]
   if len(mailingArr) > 1: #Confirm Locale is present
-    mailingAddress["locale"] = mailingArr[LOCALE]
+    street_address["locale"] = mailingArr[LOCALE]
     
   postal_code_match = re.search(POSTAL_REGEX, mailingStr)
   if postal_code_match:
     sub_string = postal_code_match.group()
-    mailingAddress["postal_code"] = sub_string
-  mailingAddress["province"] = "BC"
-  return mailingAddress
-  
+    street_address["postal_code"] = sub_string
+  street_address["province"] = "BC"
+  return street_address
+
+#############################################################################
+# Returns Locale data from Soup object
+#############################################################################
+def getLocale(soup):
+  location = soup.find('h1', string=lambda text: text is not None)
+  return location.text.removeprefix("Service BC Location: ")
+
 def extractData(url):
   STREET = 0
-  MAILING = 1
   locationData = {}
   soup = soupifyDocument(url)
   # Find the desired data
+  locationData["locale"] = getLocale(soup)
   contacts = soup.find('div', class_='contacts')
   fax = contacts.find('div', itemprop="faxNumber")
   streetsCollection = contacts.find_all('pre', itemprop="streetAddress")
@@ -75,12 +95,22 @@ def extractData(url):
   if phone is not None: 
     locationData["phone"] = phone.text.strip()
   if streetsCollection is not None and len(streetsCollection) > 0: #Verify 
-    locationData["street"] = streetsCollection[STREET].text.strip();
-    if len(streetsCollection) > 1: #Mailing address follows the same category as regular address, must check it exists
-      locationData["mailing_address"] = refineMailingAddress(streetsCollection[MAILING].text)
+    locationData["address"] = refineMailingAddress(streetsCollection[STREET].text)
+  fetchFromAPI(streetsCollection[STREET].text, locationData["locale"])
+    # if len(streetsCollection) > 1: #Mailing address follows the same category as regular address, must check it exists
+  locationData["province"] = "BC"
+  
   return locationData
 
 links = getInitialURLs(soupifyDocument(BASE_URL+START_POINT))
 
-for link in links:
-  print(extractData(link))
+results = []
+
+jsonDoc = open(RESULTS_FILE, "w+")
+# for link in links:
+for i in range (5):
+  results.append(extractData(links[i]))
+
+#Fire Data from the scraper into a results file, pretty printed
+jsonDoc.write(json.dumps(results, indent=2));
+
