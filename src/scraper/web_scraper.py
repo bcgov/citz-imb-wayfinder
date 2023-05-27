ad089d2544168dd300a22ea24f9ab397e2480cc1
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-05-26 21:37:26 -0700

    Scrape Services, Prettified the CLI Output

--- src/scraper/web_scraper.py
+++ src/scraper/web_scraper.py
@@ -1,17 +1,27 @@
+#############################################################################
+# PURPOSE: To assist Project Wayfinder in collecting and holding data about
+#          BC Services locations. This Script leverages the positionStack API
+#          to gather latitude and longtitude the information scraped, as well as 
+#          add additional details regarding locations.
+#
+# USE:     Using the Script is simple, run pipenv install and pipenv shell in terminal
+#          to have all extra libraries imported and running
+#############################################################################
+
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #Web Scraper Library
 import json
 import re
 from dotenv import dotenv_values
-import urllib
+import urllib #Encodes URLs for HTTP requests 
+from tqdm import tqdm # Gives the CLI progress bar
 
 CONFIG = dotenv_values(".env")
 
-#Constants for beginning search and filtering initial results
 BASE_URL = "https://www2.gov.bc.ca"
 START_POINT = '/gov/content/governments/organizational-structure/ministries-organizations/ministries/citizens-services/servicebc'
 FILTER = "/gov/content/governments/organizational-structure/ministries-organizations/ministries/citizens-services/servicebc/service-bc-location-"
-RESULTS_FILE="results.json"
+OUTPUT_FILE="results.json"
 
 #############################################################################
 # @desc : Given a URL, requests the data and returns the soup object
@@ -28,7 +38,7 @@ def soupifyDocument(url):
 # @desc : Taking the Soup'd content of the original URL, creates an array of URL's
 # @return : Array of URL's to further parse out
 #############################################################################
-def getInitialURLs(soup):
+def scrape_initial_urls(soup):
   destinations = []
   initialLinks = soup.find_all(
     "a", href=lambda href: href is not None and FILTER in href
@@ -41,9 +51,10 @@ def getInitialURLs(soup):
   return destinations
 
 #############################################################################
-# @desc: Take current 
+# @desc: Take the location string scraped, refined with the locale, and gather
+#        additional details from a third party API 
 #############################################################################
-def fetchFromAPI(locationString, localeString):
+def get_supplementary_api_data(locationString, localeString):
   queryString = f"{locationString}, {localeString}, ca"
   response = requests.get(CONFIG['API_URL'] + urllib.parse.quote_plus(queryString))
   return response.json()
@@ -53,17 +64,9 @@ def fetchFromAPI(locationString, localeString):
 #        be more granular
 # @returns: Dictionary Object of broken down string
 #############################################################################
-def refineMailingAddress(mailingStr):
-  ADDRESS = 0
-  LOCALE = 1
+def scrape_postal_code(mailingStr):
   POSTAL_REGEX = r'[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d'
   street_address = {}
-  mailingArr = mailingStr.strip().split(',')
-  if len(mailingArr) > 0: #Confirm Address is present
-    street_address["address"] = mailingArr[ADDRESS]
-  if len(mailingArr) > 1: #Confirm Locale is present
-    street_address["locale"] = mailingArr[LOCALE]
-    
   postal_code_match = re.search(POSTAL_REGEX, mailingStr)
   if postal_code_match:
     sub_string = postal_code_match.group()
@@ -72,45 +75,93 @@ def refineMailingAddress(mailingStr):
   return street_address
 
 #############################################################################
-# Returns Locale data from Soup object
+# @desc: Parses Locale data from Soup object
+# @returns: Locale String
 #############################################################################
-def getLocale(soup):
+def scrape_locale(soup):
   location = soup.find('h1', string=lambda text: text is not None)
   return location.text.removeprefix("Service BC Location: ")
 
-def extractData(url):
+#############################################################################
+# @desc: Parses the Web pages for available services offered by each 
+#        Service BC Location
+# @returns: [String] Services list
+#############################################################################
+def scrape_services(soup):
+  services = []
+  service_divs = soup.find_all("div", class_="panel-text")
+  for service_div in service_divs:
+    header_two = service_div.find('h2', class_="bc_h2")
+    
+    if header_two is not None and "Services Available at this Location:" in header_two.text: 
+      ul = service_div.find_all("ul")
+      for li in ul:
+        service_list_items = li.find_all("li")
+        for service_list_item in service_list_items:
+          if service_list_item is not None:
+            split_service = service_list_item.text.split("\n")
+            for splits in split_service:
+              if splits != "\t" and len(splits) > 0:
+                services.append(splits)
+  return list(set(services))
+    
+#############################################################################
+# @desc - extract data from the URL's and parse them for location data 
+#         using an auxiliary API, gathers more information from whats gathered
+#         to help standardize the data available
+#############################################################################
+def scrape_url(url):
   STREET = 0
   locationData = {}
+  locationData["contact"] = {}
+  
   soup = soupifyDocument(url)
-  # Find the desired data
-  locationData["locale"] = getLocale(soup)
+  locationData["services"] = scrape_services(soup)
+  # Extract desired data
+  locationData["locale"] = scrape_locale(soup)
+  locationData["website"] = url
   contacts = soup.find('div', class_='contacts')
   fax = contacts.find('div', itemprop="faxNumber")
   streetsCollection = contacts.find_all('pre', itemprop="streetAddress")
-  phone = contacts.find('div', itemprop="telephone")  
-
-  #Verify data exists individually
-  if fax is not None:
-    locationData["fax"] = fax.text.strip()
-  if phone is not None: 
-    locationData["phone"] = phone.text.strip()
-  if streetsCollection is not None and len(streetsCollection) > 0: #Verify 
-    locationData["address"] = refineMailingAddress(streetsCollection[STREET].text)
-  fetchFromAPI(streetsCollection[STREET].text, locationData["locale"])
-    # if len(streetsCollection) > 1: #Mailing address follows the same category as regular address, must check it exists
-  locationData["province"] = "BC"
+  phone = contacts.find('div', itemprop="telephone")
   
+  # Verify data was extracted in scrape
+  if fax is not None:
+    locationData["contact"]["fax"] = fax.text.strip() or ""
+  if phone is not None: 
+    locationData["contact"]["phone"] = phone.text.strip() or ""
+  if streetsCollection is not None and len(streetsCollection) > 0: #Verify 
+    locationData["address"] = scrape_postal_code(streetsCollection[STREET].text) or ""
+
+  # Use scraped data to gather meta data
+  supplement_data = get_supplementary_api_data(streetsCollection[STREET].text, locationData["locale"])
+
+  # Verify the API returned valid data, then take whats important
+  if len(supplement_data['data']) > 0:
+    locationData["latitude"] = supplement_data['data'][0]['latitude'] or ""
+    locationData["longitude"] = supplement_data['data'][0]['longitude'] or ""
+    locationData["address"]["region"] = supplement_data['data'][0]['region'] or ""
+    locationData["address"]["county"] = supplement_data['data'][0]['county'] or ""
+    locationData["address"]["locality"] = supplement_data['data'][0]['locality'] or ""
+    locationData["address"]["label"] = supplement_data['data'][0]['label'] or ""  
+
   return locationData
 
-links = getInitialURLs(soupifyDocument(BASE_URL+START_POINT))
+try:
+  print("\033[1;32m Beginning Scrape of BC Services")
+  #Create or overwrite output file
+  jsonDoc = open(OUTPUT_FILE, "w+")
+  links = list(set(scrape_initial_urls(soupifyDocument(BASE_URL+START_POINT))))
+  results = []
 
-results = []
-
-jsonDoc = open(RESULTS_FILE, "w+")
-# for link in links:
-for i in range (5):
-  results.append(extractData(links[i]))
-
-#Fire Data from the scraper into a results file, pretty printed
-jsonDoc.write(json.dumps(results, indent=2));
-
+  # for link in links:
+  for i in tqdm(range(0, len(links)), ncols=100, colour="#669966", desc = "Progress: "):
+    results.append(scrape_url(links[i]))
+    
+  # Format Data into JSON, Pretty print, and write to Output file
+  jsonDoc.write(json.dumps(results, indent=2));
+  print(f"\033[1;32m Scraping successfully completed, view results in {OUTPUT_FILE}")
+except Exception as err:
+  print(f"\033[1;31m err")
+finally:
+  print("Script Terminated")
