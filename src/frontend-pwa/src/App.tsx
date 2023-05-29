dffc1c6fc5936b6bfdf0365d52d07739fb0ce2aa
TylerMaloney <123214152+TylerMaloney@users.noreply.github.com>
2023-05-29 09:22:11 -0700

    removed button placeholder

--- src/frontend-pwa/src/App.tsx
+++ src/frontend-pwa/src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { Button } from './components/Button/Button';
 
 function App() {
-  function handleClick() {
-    console.log('Hello! IMB is cool');
-  }
   return (
     <div>
       <Header />
-      <Button handleClick={() => handleClick()} variant="primary" size="sm" disabled={false} />
       <Footer />
     </div>
   );
