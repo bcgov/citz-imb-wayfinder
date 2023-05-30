4df41d7bdabb577cbf3713066e4efda0bfb800f4
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-30 13:08:42 -0700

    Fixed unused Textfield import in Report View

--- src/frontend-pwa/src/views/Report/Report.tsx
+++ src/frontend-pwa/src/views/Report/Report.tsx
@@ -2,8 +2,11 @@ import { TextField } from '@mui/material';
 
 export default function Report() {
   return (
-    <h2>
-      Report an Event
-    </h2>
+    <div>
+      <h2>
+        Report an Event
+      </h2>
+      <TextField />
+    </div>
   );
 }
