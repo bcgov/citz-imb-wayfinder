6f8c6b2e0639a98e08600f5507ca238b7ed00649
TylerMaloney <tyler.maloney010@gmail.com>
2023-05-31 16:16:52 -0700

    eula stub commit

---
+++ src/frontend-pwa/src/views/Eula/Eula.tsx
@@ -0,0 +1,9 @@
+import { StyledContainer } from './eula.styles';
+
+export default function Eula() {
+  return (
+    <StyledContainer className="eula">
+      Find a BC Service
+    </StyledContainer>
+  );
+}
---
+++ src/frontend-pwa/src/views/Eula/eula.styles.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const StyledContainer = styled.div`
+    min-width: 100%;
+    min-height: 100%;
+    background-color: cyan;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const p = styled.p`
+`;
