4cb2d235cef4df8bb80c857d2d00b557feff2a02
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-30 22:16:46 -0700

    Basic SplashScreen html and css written

---
+++ src/frontend-pwa/src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,13 @@
+import logo from '/logo-banner.svg';
+
+export default function SplashScreen() {
+  return (
+    <div>
+      <img src={logo} alt="BC Gov logo" />
+      <div />
+      <p>
+        Loading
+      </p>
+    </div>
+  );
+}
---
+++ src/frontend-pwa/src/components/SplashScreen/splashscreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import typography from '../../typography';
+
+export const Wrapper = styled.div`
+  background-color: #036;
+`;
+
+export const Text = styled.p`
+${typography.toString()}  
+`;
