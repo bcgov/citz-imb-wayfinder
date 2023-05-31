f0da623eead741aec5b5da24bfbb16745011e56a
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-30 20:15:42 -0700

    Basic slider component created

---
+++ src/frontend-pwa/src/components/Slider/Slider.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export type SliderProps = {
+  min: number;
+  max:number;
+  onChange: (value: number) => void;
+}
+
+export default function Slider({
+  min,
+  max,
+  onChange,
+}:SliderProps) {
+  const [value, setValue] = useState(min);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value, 10);
+    setValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <input
+      type="range"
+      min={min}
+      max={max}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+}
