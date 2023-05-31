0143d24ec9ed5f82071c710fbe8dabfab508aed1
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-05-30 23:08:26 -0700

    Updated App to conditionally render splashscreen on load

--- src/frontend-pwa/src/App.tsx
+++ src/frontend-pwa/src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import ViewRouter from './routes/ViewRouter';
 import SplashScreen from './components/SplashScreen/SplashScreen';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <Header />
-      <ViewRouter />
-      <Footer />
+      {isLoading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Header />
+          <ViewRouter />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
