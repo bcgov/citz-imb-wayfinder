af36a83a3c3a0296e938423789eb0b13880e0975
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-01 13:44:27 -0700

    Settings Styled

---
+++ src/frontend-pwa/src/views/Settings/settings.styles.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import typography from '../../typography';
+
+export const SettingsContainer = styled.div`
+  width: 100vw;
+  padding: 100px;
+  border-radius: 4px;
+  position: fixed;
+  top: 10%;
+  left: 0%;
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+`;
+
+export const Section = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h2`
+  ${typography.toString()}
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
