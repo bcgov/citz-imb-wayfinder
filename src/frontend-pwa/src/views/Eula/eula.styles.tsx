fba82daf0fd3bfebb7e017430037a6fc8804cb92
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-01 14:47:25 -0700

    added eula styling and functionality

--- src/frontend-pwa/src/views/Eula/eula.styles.tsx
+++ src/frontend-pwa/src/views/Eula/eula.styles.tsx
@@ -1,12 +1,49 @@
 import styled from '@emotion/styled';
+import typography from '../../typography';
 
 export const StyledContainer = styled.div`
-    min-width: 100%;
-    min-height: 100%;
-    background-color: cyan;
-    display: flex;
+    ${typography.toString()}
+    width: 100%;
+    height: 100%;
+    flex: auto;
+    flex-direction: column;
+    background-color: white;
     align-items: center;
     justify-content: center;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    display: inline-block;
+    padding: 5em 10em;
+
+`;
+// changed below "max-height" from "min-height"
+
+export const StyledOuterDiv = styled.div`
+    width: 100%;
+    max-height: 100%;
+    min-width: 0;
+    min-height: 0;
+    display: flex;
+    flex-direction: column;
+    background-color: #003366;
+    align-items: center;
+    justify-content: center;
+    overflow-y: hidden;
+    padding: 4em 0em;
+
+`;
+
+export const StyledFieldSetDiv = styled.div`
+${typography.toString()}
+    width: 100%;
+    height: 100%;
+    flex: auto;
+    flex-direction: row;
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    display: inline-block;
+    padding: 1em 11em;
 `;
 
 export const p = styled.p`
