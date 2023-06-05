65bc85fe3507608624e4c8a0e25994cf3f9e59db
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-05 10:49:02 -0700

    Added comment to ListItems.tsx and ListItems.styles.ts

--- src/frontend-pwa/src/components/ListItems/ListItems.tsx
+++ src/frontend-pwa/src/components/ListItems/ListItems.tsx
@@ -1,3 +1,11 @@
+/**
+ * @summary A reusable component that creates a list of ListItem components
+ * @param items - is an array of Single locations and their data
+ * @param currentLocation - is the current location of the user's device
+ * @type {(items : Attay<SingleLocation>, currentLocation : CurrentLocation)}
+ * @author Dallas Richmond
+ */
+
 /* eslint-disable max-len */
 /* eslint-disable react/no-array-index-key */
 import ListItem from '../ListItem/ListItem';
--- src/frontend-pwa/src/components/ListItems/listitems.style.ts
+++ src/frontend-pwa/src/components/ListItems/listitems.style.ts
@@ -1,3 +1,7 @@
+/**
+ * @summary Styles for ListItems component
+ * @author Dallas Richmond
+ */
 import styled from '@emotion/styled';
 import typography from '../../typography';
 
