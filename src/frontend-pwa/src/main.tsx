6a64e5990dc44debabf5520fe3d1dff36d7bba51
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-06-05 23:00:40 -0700

    Added features to webmanifest

--- src/frontend-pwa/src/main.tsx
+++ src/frontend-pwa/src/main.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css';
+
+const updateSW = registerSW({
+  onOfflineReady() { },
+});
+if ('serviceWorker' in navigator) {
+  updateSW();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
