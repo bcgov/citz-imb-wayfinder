157cc9304aa0ebfa4b925ced2fdaeab89ddffdca
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-06-06 16:22:07 -0700

    CORS Updates

--- src/api/config/corsConfig.ts
+++ src/api/config/corsConfig.ts
@@ -4,7 +4,9 @@
  */
 const corsConfig = {
   origin: [
-    'https://wayfinder.apps.silver.devops.gov.bc.ca/',
+    'https://wayfinder.apps.silver.devops.gov.bc.ca',
+    'http://localhost:5173',
+    'http://localhost:4173',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
