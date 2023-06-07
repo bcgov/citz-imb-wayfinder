4d8b842d7a98628ed92b2f483a1ea54ad239bb45
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-06-06 20:25:55 -0700

    Updated LocationController + Swagger

--- src/api/controllers/location-controller.ts
+++ src/api/controllers/location-controller.ts
@@ -1,21 +1,43 @@
 /**
- * @summary Location Endpoint will send all locations
+ * @summary Location Endpoint will send all locations and unique services
  *          (Offline functionality requires having locations cached)
  * @author  LocalNewsTV
  */
 import mongoose from 'mongoose';
 import { Request, Response } from 'express';
+import SingleLocation from '../types/SingleLocation';
 
 const locationsModel = mongoose.model('location');
 
+type ResponseObject = {
+  locations: Array<SingleLocation>;
+  services: Array<string>;
+}
 /**
- * @desc returns a collection of all Locations in BC from database
- * @returns {Array} Array of Sites by Location
+ * @desc Takes all SingleLocations from database then creates a unique
+ *       list of alphabetically organized services. Data is used by Wayfinder
+ *       application to seed the cache.
+ * @summary sends seed data for frontend
+ * @example {
+ *  "locations": [{...}],
+ *  "services" : [""]
+ * }
+ * @returns {ResponseObject} Array of Sites by Location
  */
-const getAllLocations = async (req: Request, res: Response): Promise<void> => {
-  await locationsModel.find()
-    .then((response) => res.status(200).send({ locations: response }))
-    .catch((err) => res.status(500).send(`An Error has Occured, ${err}`));
+const getAllLocations = async (req: Request, res: Response): Promise<Response> => {
+  const serviceArr: Array<string> = [];
+  const responseObject: ResponseObject = {} as ResponseObject;
+  const data: Array<SingleLocation> = await locationsModel.find();
+
+  data.forEach((location) => {
+    serviceArr.push(...location.services);
+  });
+
+  responseObject.locations = data;
+  // Create a unique list of services
+  responseObject.services = [...new Set(serviceArr.sort())];
+
+  return res.status(200).json(responseObject);
 };
 
 export default getAllLocations;
