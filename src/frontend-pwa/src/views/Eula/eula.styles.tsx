d2f6abe08f8596571d5c1f5ab6e5140090ba498a
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-07 15:51:43 -0700

    final eula touchups, changed header/footer z-index

--- src/frontend-pwa/src/views/Eula/eula.styles.tsx
+++ src/frontend-pwa/src/views/Eula/eula.styles.tsx
@@ -1,44 +1,41 @@
 import styled from '@emotion/styled';
 import typography from '../../typography';
 
-export const StyledContainer = styled.div`
-    ${typography.toString()}
-    width: 100%;
-    height: 100%;
-    flex: auto;
-    flex-direction: column;
-    background-color: white;
-    align-items: center;
-    justify-content: center;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    display: inline-block;
-    padding: 5em 7.5pt;
-    @media (max-width: 768px) {
-        padding: 20px;
-    }
-
-`;
 // changed below "max-height" from "min-height"
 
 export const StyledOuterDiv = styled.div`
     width: 100%;
-    max-height: 100%;
+    min-height: 100vh;
+    position: absolute;
+    left:0;
+    top:0;
     min-width: 0;
-    min-height: 0;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
-    background-color: #003366;
     align-items: center;
     justify-content: center;
-    overflow-y: hidden;
-    padding: 4em 0em;
-
+    padding: 50pt 0;
+`;
+export const StyledContainer = styled.div`
+    ${typography.toString()}
+    width: 100%;
+    max-width: 768px;
+    height: 100%;
+    flex-direction: column;
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    padding: 5em 7.5pt;
+    @media (max-width: 768px) {
+        padding: 20px;
+    }
 `;
 
 export const StyledFieldSetDiv = styled.div`
 ${typography.toString()}
-    width: 100%;
+    width: 288pt;
     height: 100%;
     display: flex;
     flex-direction: row;
@@ -46,6 +43,9 @@ ${typography.toString()}
     align-items: center;
     justify-content: space-evenly;
     padding: 2em 1em;
+    @media (max-width: 768px){
+        width: 100%;
+    }
 `;
 
 export const p = styled.p`
