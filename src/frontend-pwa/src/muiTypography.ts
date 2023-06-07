50692a85fe285e5fcb65ef8c3d76e82ff745668b
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-07 12:24:50 -0700

    spaghetti code, incomplete, working on other tickets

---
+++ src/frontend-pwa/src/muiTypography.ts
@@ -0,0 +1,23 @@
+// import BCSans from '/src/BCSans-Regular.woff';
+// import { createTheme } from '@mui/material';
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: 'BCSans, Arial, sans-serif',
+//     fontWeightRegular: 400,
+//   },
+//   styleOverrides: {
+//     MuiCssBaseline: {
+//       '@global': {
+//         '@font-face': {
+//           fontFamily: 'BCSans',
+//           lineHeight: 1.25,
+//           src: `url(${BCSans}) format('woff')`,
+//           // You can also specify other font formats here if necessary
+//         },
+//       },
+//     },
+//   },
+// });
+
+// export default theme;
