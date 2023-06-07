c96b3c3e5f4e8c4a8d47287b72d46d6281f9200b
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-07 15:20:08 -0700

    increased toggle checkbox size, changed some styling

--- src/frontend-pwa/src/components/Toggle/Toggle.tsx
+++ src/frontend-pwa/src/components/Toggle/Toggle.tsx
@@ -6,18 +6,15 @@
  * @author Dallas Richmond
  */
 import { useState } from 'react';
+import StyledToggle from './toggle.styles';
 
 export type ToggleProps = {
   ariaLabel: string;
   onChange: (checked: boolean) => void;
   defaultChecked: boolean;
-}
+};
 
-export default function Toggle({
-  ariaLabel,
-  onChange,
-  defaultChecked = false,
-}: ToggleProps) {
+export default function Toggle({ ariaLabel, onChange, defaultChecked = false }: ToggleProps) {
   const [checked, setChecked] = useState(defaultChecked);
 
   const handleChange = () => {
@@ -26,14 +23,5 @@ export default function Toggle({
     onChange(newChecked);
   };
 
-  return (
-    <div>
-      <input
-        aria-label={ariaLabel}
-        type="checkbox"
-        checked={checked}
-        onChange={handleChange}
-      />
-    </div>
-  );
+  return <StyledToggle aria-label={ariaLabel} type="checkbox" checked={checked} onChange={handleChange} />;
 }
