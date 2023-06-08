643675a4ba1a1e9e65607c7321476666c80c7d26
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-06-07 21:08:29 -0700

    Update NavButton to take size prop

--- src/frontend-pwa/src/components/NavButton/NavButton.tsx
+++ src/frontend-pwa/src/components/NavButton/NavButton.tsx
@@ -2,28 +2,31 @@
  * @summary A reusable nav button component used for routing to different pages
  * @param path - is the path the Link will route to
  * @param text - is the text displayed on the button
+ * @param size - The predetermined size prop for the button
  * @type {(path: string, text: string)}
  * @author Dallas Richmond
  */
 import {
   Link,
 } from 'react-router-dom';
-import { Button } from '../Button/Button';
+import { Button, ButtonSizes } from '../Button/Button';
 
 export type RoutingLinkProps = {
   path: string;
   text: string;
+  size: ButtonSizes;
 }
 
 export default function NavButton({
   path,
   text,
+  size = 'lg',
 }: RoutingLinkProps) {
   return (
     <Link to={path}>
       <Button
         variant="primary"
-        size="lg"
+        size={size}
         disabled={false}
         text={text}
       />
