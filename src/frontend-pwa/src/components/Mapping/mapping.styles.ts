724db8c29adbbe89a50a6673dfa7089e85e8e32e
Matthew Logan <loganmatthewr@gmail.com>
2023-06-09 15:13:45 -0700

    Stub Map Component creation

---
+++ src/frontend-pwa/src/components/Mapping/mapping.styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+// import typography from '../../typography';
+
+export const StyledMap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+`;
+export const StyledImg = styled.img`
+
+`;
