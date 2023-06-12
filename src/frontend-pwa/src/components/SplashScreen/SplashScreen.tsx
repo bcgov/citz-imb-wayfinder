7470c9eaa71fdfce01a64116f5f7aa761413067b
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-12 15:57:11 -0700

    OnlineCheck.ts added to check it online

--- src/frontend-pwa/src/components/SplashScreen/SplashScreen.tsx
+++ src/frontend-pwa/src/components/SplashScreen/SplashScreen.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import logo from '/bc-logo-vertical.svg';
 import Spinner from '../Spinner/Spinner';
 import useAppService from '../../services/app/useAppService';
+import OnlineCheck from '../../utils/OnlineCheck';
 import {
   SplashScreenWrapper,
   Image,
@@ -14,18 +15,25 @@ import {
 
 export default function SplashScreen() {
   const {
-    state, setOnlineAppData, setOfflineAppData, setCurrentLocation, setLoading,
+    setOnlineAppData, setOfflineAppData, setCurrentLocation, setLoading,
   } = useAppService();
 
   useEffect(() => {
     // Todo: Need to properly implement Online Offline. Navigator.onLine won't be enough
     // Change setOnlineAppData and setOfflineAppData to setAppData and pass in a isOnline prop
-    if (navigator.onLine && Object.keys(state.appData).length === 0) {
-      setOnlineAppData();
-      setCurrentLocation();
-    } else if (Object.keys(state.appData).length === 0) {
-      setOfflineAppData();
-    }
+    OnlineCheck()
+      .then((online) => {
+        if (online) {
+          setOnlineAppData();
+          setCurrentLocation();
+        } else if (!online) {
+          setOfflineAppData();
+        }
+        console.log(online);
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
     setLoading(false);
   });
 
