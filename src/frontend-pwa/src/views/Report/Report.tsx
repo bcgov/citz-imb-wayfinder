7712a6e508d7544e80b02ed24400a75fa34255ba
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-14 14:28:28 -0700

    why stash when you can commit

--- src/frontend-pwa/src/views/Report/Report.tsx
+++ src/frontend-pwa/src/views/Report/Report.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { Button } from '../../components/Button/Button';
+import { StyledReportContainer, StyledReportOuterDiv } from './report.styles';
 
 export function Report() {
   const [eventType, setEventType] = useState('');
   const [eventInfo, setEventInfo] = useState('');
+  const eventOptions: Array<string> = ['Damaged Infrastructure', 'Animal Sighting', 'Suggestion/Complaint', 'Miscellaneous'];
 
   const handleEventTypeChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setEventType(e.target.value);
@@ -24,25 +27,32 @@ export function Report() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="eventType">Event Type:</label>
-        <select id="eventType" value={eventType} onChange={handleEventTypeChange}>
-          <option value="">Select an event type</option>
-          <option value="event1">Event 1</option>
-          <option value="event2">Event 2</option>
-          <option value="event3">Event 3</option>
-          <option value="event4">Event 4</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="eventInfo">Event Information:</label>
-        <textarea id="eventInfo" value={eventInfo} onChange={handleEventInfoChange} rows={4} />
-      </div>
+      <StyledReportOuterDiv>
+        <StyledReportContainer>
+          <h1>Report An Event</h1>
+          <label htmlFor="eventType">
+            <div>Event Type:</div>
+            <select id="eventType" value={eventType} onChange={handleEventTypeChange}>
+              <option value="">Select an event type</option>
+              {eventOptions.map((event: string, index: number) => (
+                <option value={index}>{event}</option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label htmlFor="eventInfo">
+            <div>Event Information:</div>
+            <textarea id="eventInfo" value={eventInfo} onChange={handleEventInfoChange} rows={10} cols={30} />
+          </label>
+          <br />
+          <Button text="Submit" variant="primary" size="md" disabled={false} />
+        </StyledReportContainer>
+      </StyledReportOuterDiv>
       <div>
         <button type="submit">Submit</button>
       </div>
     </form>
   );
-};
+}
 
 export default Report;
