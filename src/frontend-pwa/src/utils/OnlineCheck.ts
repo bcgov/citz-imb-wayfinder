3e76d2f962ab84a881c7b7dece06d2a2af14a650
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-13 18:12:21 -0700

    Added comment to OnlineCheck

--- src/frontend-pwa/src/utils/OnlineCheck.ts
+++ src/frontend-pwa/src/utils/OnlineCheck.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import constants from '../constants/Constants';
 
+/**
+ * @summary Pings the API's health endpoint to determine if the app is online or not
+ * @returns a boolean that indicates whether the App is online or not
+ * @author Dallas Richmond
+ */
 const OnlineCheck = async () => {
   try {
     const check = await axios.get(`${constants.BACKEND_URL}/api/health`);
