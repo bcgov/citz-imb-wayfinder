630f8d9e43f0a137879da411165be041e4eaa46f
TylerMaloney <tyler.maloney010@gmail.com>
2023-06-14 14:17:47 -0700

    fixed constants

--- src/frontend-pwa/src/constants/Constants.ts
+++ src/frontend-pwa/src/constants/Constants.ts
@@ -1,5 +1,5 @@
 const constants = {
-  BACKEND_URL: !import.meta.env.DEV ? 'http://localhost:3000' : 'https://wf-api-ec1236-dev.apps.silver.devops.gov.bc.ca',
+  BACKEND_URL: import.meta.env.DEV ? 'http://localhost:3000' : 'https://wf-api-ec1236-dev.apps.silver.devops.gov.bc.ca',
   VER_KEY: 'V0.0.0',
   APP_DATA_KEY: 'appData',
   CURRENT_LOCATION_KEY: 'currentLocation',
