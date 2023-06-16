4f36294f29999274ea687e54fe03bbaf89e9c008
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-16 11:04:24 -0700

    Search by location added

--- src/frontend-pwa/src/views/Location/Location.tsx
+++ src/frontend-pwa/src/views/Location/Location.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /**
  * @summary This is the main location view for mapping
  * @param locations - is an array of single locations pulled from the /location endpoint
@@ -9,30 +10,57 @@ import ListItems from '../../components/ListItems/ListItems';
 import SingleLocation from '../../Type/SingleLocation';
 import useAppService from '../../services/app/useAppService';
 import LocationListItem from '../../components/LocationListItem/LocationListItem';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import Mapping from '../../components/Mapping/Mapping';
 
 import {
-  LocationViewWrapper,
-  TextHeader,
+  ContentContainer,
+  MapContainer,
+  ViewContainer,
+  ServiceListContainer,
+  StyledP,
 } from './location.styles';
 
 export default function Location() {
   const { state } = useAppService();
   const [searchQuery, setSearchQuery] = useState('');
   const locations = state.appData?.data ? state.appData.data.serviceBCLocations : [];
+  const filteredLocationSearch = locations.filter((location : SingleLocation) => (
+    location.locale.toLowerCase().match(`${searchQuery.toLowerCase().trim()}`)
+  ));
 
   return (
-    <LocationViewWrapper>
-      <TextHeader>
-        Locate a Service
-      </TextHeader>
-      <ListItems
-        headers={['Locations', 'Distance']}
-      >
-        {locations.map((data: SingleLocation, index: number) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <LocationListItem itemData={data} key={index} />
-        ))}
-      </ListItems>
-    </LocationViewWrapper>
+    <ViewContainer>
+      <ContentContainer>
+        {navigator.onLine
+          ? (
+            <Mapping
+              locations={filteredLocationSearch}
+              currentLocation={state.currentLocation}
+            />
+          )
+          : (
+            <MapContainer>
+              <StyledP>
+                Sorry this feature is currently unavailable offline.
+              </StyledP>
+            </MapContainer>
+          )}
+        <ServiceListContainer>
+          <SearchBar
+            query={searchQuery}
+            setUseState={setSearchQuery}
+            handleClear={() => setSearchQuery('')}
+            border={false}
+            borderRadius={false}
+          />
+          <ListItems headers={['Locations', 'Distance']}>
+            {filteredLocationSearch.map((data: SingleLocation, index: number) => (
+              <LocationListItem itemData={data} key={index} />
+            ))}
+          </ListItems>
+        </ServiceListContainer>
+      </ContentContainer>
+    </ViewContainer>
   );
 }
--- src/frontend-pwa/src/views/Location/location.styles.ts
+++ src/frontend-pwa/src/views/Location/location.styles.ts
@@ -3,17 +3,63 @@
  * @author Dallas Richmond
  */
 import styled from '@emotion/styled';
-import typography from '../../typography';
 
-export const LocationViewWrapper = styled.div`
-  width: 100vw;
-  padding: 100px;
-  @media (max-width: 768px) {
-    padding: 20px;
+export const ViewContainer = styled.div`
+  background-color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  min-height: 100svh;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  padding: 50pt 0pt;
+  flex-direction: column;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  max-width: 1250px;
+  overflow: hidden;
+  @media (min-width: 768px) {
+    flex-direction: row-reverse;
+    padding: inherit 10pt;
   }
 `;
 
-export const TextHeader = styled.h2`
-  ${typography.toString()}
-  font-weight: 500;
+export const ServiceListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 40vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 768px) {
+    height: 100%;
+    max-height: 100%;
+    width: 425pt;
+    margin: 0 1em;
+  }
+`;
+
+export const MapContainer = styled.div`
+  display: flex;
+  height: 40svh;
+  min-height: 40svh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 768px) {
+    height: 100%;
+    border-radius: 4pt;
+  }
+`;
+
+export const StyledP = styled.p`
+  width: 100%;
+  text-align: center;
 `;
