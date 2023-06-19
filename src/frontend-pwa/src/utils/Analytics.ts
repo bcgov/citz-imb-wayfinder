0d94cf354ec3d5d6ade453029ed8f81670c4e3e2
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-19 15:45:35 -0700

    Added test calls

--- src/frontend-pwa/src/utils/Analytics.ts
+++ src/frontend-pwa/src/utils/Analytics.ts
@@ -1,7 +1,23 @@
-export const ReportAnalytics = () => {
+/* eslint-disable no-console */
+import axios from 'axios';
+import constants from '../constants/Constants';
 
+export const ReportAnalytics = (request: object) => {
+  axios.post(`${constants.BACKEND_URL}/api/analytic`, request)
+    .then((data) => {
+      console.log('Data: ', data);
+    })
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
 };
 
-export const LocationAnalytics = () => {
-
+export const LocationAnalytics = (request: object) => {
+  axios.post(`${constants.BACKEND_URL}/api/analytic`, request)
+    .then((data) => {
+      console.log('Data: ', data);
+    })
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
 };
