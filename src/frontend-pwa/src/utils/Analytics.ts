9768d532da6d728c0bcab77b95f32158c670db4b
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-06-19 19:17:33 -0700

    Comments added to Analytics

--- src/frontend-pwa/src/utils/Analytics.ts
+++ src/frontend-pwa/src/utils/Analytics.ts
@@ -1,3 +1,9 @@
+/**
+ * @summary Function that posts the request to the analytics endpoint
+ * @param request is the request to be posted
+ * @type {(request: object)}
+ * @author Dallas Richmond
+ */
 /* eslint-disable no-console */
 import axios from 'axios';
 import constants from '../constants/Constants';
