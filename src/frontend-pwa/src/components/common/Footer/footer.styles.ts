908f2530613e71129f3da745fbe01139ea69565b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-06-22 11:19:46 -0700

    Cleaned up order of css

--- src/frontend-pwa/src/components/common/Footer/footer.styles.ts
+++ src/frontend-pwa/src/components/common/Footer/footer.styles.ts
@@ -1,31 +1,38 @@
 /**
  * @summary Styles for Footer component
- * @author Dallas Richmond
+ * @author Dallas Richmond, LocalNewsTV
  */
 import styled from '@emotion/styled';
 import typography from '../../../typography';
+import mq from '../../../constants/mq';
 
 export const FooterWrapper = styled.footer`
   ${typography.toString()}
-  background-color: #036;
-  border-top: 2px solid #fcba19;
-  padding: 0 65px;
-  color: #fff;
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  height: 65px;
-  bottom: 0;
   position: fixed;
-  width: 100%;
+  bottom: 0;
   left: 0;
   z-index: 2000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 65px;
+  width: 100%;
+  background-color: #036;
+  color: #FFF;
+  border-top: 2px solid #FCBA19;
+  padding: 0;
 `;
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
-  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: row;
+  width: 100dvw;
+  max-width: ${mq.tablet};
   text-align: center;
   height: 46px;
+  @media (min-width: ${mq.desktop}){
+    display: none;
+  }
 `;
