bc1fd3c771c8f6f72d970523c36d0d50901a6823
Matthew Logan <loganmatthewr@gmail.com>
2023-06-26 10:52:27 -0700

    Add Greetings to main, update icons

--- src/frontend-pwa/src/views/Home/Home.tsx
+++ src/frontend-pwa/src/views/Home/Home.tsx
@@ -7,38 +7,50 @@ import { NavButton } from '../../components/appNav';
 import {
   Wrapper,
   ButtonWrapper,
+  ViewContainer,
+  GreetingWrapper,
 } from './home.styles';
-import location from '/iconography/FindOfficeColor.svg';
+import location from '/iconography/FindOffice.svg';
 import report from '/iconography/ReportColor.svg';
 import services from '/iconography/FindServiceColor.svg';
 
 export default function Home() {
+  const greetings = [
+    'Welcome, what are you looking to do?',
+    'Let\'s get started',
+    'What are you looking for today?',
+    'Connecting citizens to services',
+  ];
+  const currentGreeting = greetings[Math.floor(Math.random() * greetings.length)];
   return (
-    <Wrapper>
-      <ButtonWrapper>
-        <NavButton
-          path="/location"
-          text="Find an Office"
-          icon={location}
-          hex="#DEF3FB"
-        />
-      </ButtonWrapper>
-      <ButtonWrapper>
-        <NavButton
-          path="/services"
-          text="Find BC Services"
-          icon={services}
-          hex="#EEE0E9"
-        />
-      </ButtonWrapper>
-      <ButtonWrapper>
-        <NavButton
-          path="/report"
-          text="Report an Event"
-          icon={report}
-          hex="#FEF2DF"
-        />
-      </ButtonWrapper>
-    </Wrapper>
+    <ViewContainer>
+      <Wrapper>
+        <GreetingWrapper>{currentGreeting}</GreetingWrapper>
+        <ButtonWrapper>
+          <NavButton
+            path="/location"
+            text="Find an Office"
+            icon={location}
+            hex="#DEF3FB"
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <NavButton
+            path="/services"
+            text="Find BC Services"
+            icon={services}
+            hex="#EEE0E9"
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <NavButton
+            path="/report"
+            text="Report an Event"
+            icon={report}
+            hex="#FEF2DF"
+          />
+        </ButtonWrapper>
+      </Wrapper>
+    </ViewContainer>
   );
 }
--- src/frontend-pwa/src/views/Home/home.styles.ts
+++ src/frontend-pwa/src/views/Home/home.styles.ts
@@ -1,14 +1,38 @@
 /**
  * Styling for Home.tsx view
- * @author Dallas Richmond
+ * @author Dallas Richmond, LocalNewsTV
  */
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const ViewContainer = styled.div`
+    background-color: white;
+    position: absolute;
+    top: 0;
+    left: 0;
     display: flex;
-    flex-direction: column;
-    height: 100vh;
+    min-height: 100svh;
+    align-items: center;
     justify-content: center;
+    width: 100%;
+`;
+
+export const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export const GreetingWrapper = styled.p`
+    display: flex;
+    width: 100%;
+    margin: -2em 0 0.5em 0;
+    font-size: 14pt;
+    min-height: 2em;
+    color: #036;
+    justify-content: center;
+    font-style: italic;
 `;
 
 export const ButtonWrapper = styled.div`
