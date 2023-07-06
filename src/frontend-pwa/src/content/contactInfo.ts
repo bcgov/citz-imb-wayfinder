9b3901e5138640ab0d9476371e7c4756245e3514
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-07-05 21:57:26 -0700

    Moved contactInfo from AboutContact into content folder

---
+++ src/frontend-pwa/src/content/contactInfo.ts
@@ -0,0 +1,8 @@
+const contactInfo = {
+  email: 'wayfinderdevelopers@gmail.com',
+  repo: 'https://github.com/bcgov/citz-imb-wayfinder',
+  mailing: '4000 Seymour, Victoria, V8X\u00A04S7',
+  team: 'Team Wayfinder',
+};
+
+export default contactInfo;
