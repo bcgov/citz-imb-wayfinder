311b63fbef17c0ee3d7c1c026fb9c8fb48866434
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-07-07 15:28:38 -0700

    Added some tests

--- src/frontend-pwa/cypress.config.ts
+++ src/frontend-pwa/cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5173/?geolocation=true',
     experimentalStudio: true,
-
+    chromeWebSecurity: false,
   },
 });
