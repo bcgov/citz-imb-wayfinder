f0ba7c11d99dea29738b929ec5ee6909119d2389
TylerMaloney <tyler.maloney010@gmail.com>
2023-07-07 15:38:27 -0700

    combined OfflineMapping and Mapping into Mapping

--- src/frontend-pwa/src/components/utility/index.ts
+++ src/frontend-pwa/src/components/utility/index.ts
@@ -1,11 +1,9 @@
 import Mapping from './Mapping/Mapping';
 import SplashScreen from './SplashScreen/SplashScreen';
 import BannerTip from './BannerTip/BannerTip';
-import OfflineMapping from './OfflineMapping/OfflineMapping';
 
 export {
   Mapping,
-  OfflineMapping,
   SplashScreen,
   BannerTip,
 };
