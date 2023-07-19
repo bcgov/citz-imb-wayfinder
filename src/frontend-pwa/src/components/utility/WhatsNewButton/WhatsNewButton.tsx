6bfd70f201acb564ea1e32ea6a1051550dadd1ee
Matthew Logan <loganmatthewr@gmail.com>
2023-07-19 10:39:48 -0700

    stubs created

---
+++ src/frontend-pwa/src/components/utility/WhatsNewButton/WhatsNewButton.tsx
@@ -0,0 +1,11 @@
+import WNButton from './whatsNewButton.styles';
+
+function WhatsNewButton() {
+  return (
+    <WNButton
+      value="What's new"
+    />
+  );
+}
+
+export default WhatsNewButton;
---
+++ src/frontend-pwa/src/components/utility/WhatsNewButton/whatsNewButton.styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const WNButton = styled.button`
+  border: none;
+  background-color: inherit;
+`;
+
+export default WNButton;
