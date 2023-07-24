5554a32798b66c1d1b401e5fbb14e4eb5b9e25d5
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-07-24 10:16:31 -0700

    Reverted Settings

--- src/frontend-pwa/src/views/Settings/Settings.tsx
+++ src/frontend-pwa/src/views/Settings/Settings.tsx
@@ -25,29 +25,19 @@ import {
 import useAppService from '../../services/app/useAppService';
 import MoreInfoButton from '../../components/common/MoreInfoButton/MoreInfoButton';
 import { SettingsContent } from '../../content/content';
-import { localStorageKeyExists } from '../../utils/AppLocalStorage';
-import constants from '../../constants/Constants';
-import OnlineCheck from '../../utils/OnlineCheck';
-import Analytic from '../../Type/Analytic';
 
 export default function Settings() {
   const {
     setAppData,
     setSettings,
     updateSettings,
-    setAnalytics,
     state,
   } = useAppService();
   const [locationRangeValue, setLocationRangeValue] = useState(state.settings.location_range);
   const [offlineToggleValue, setOfflineToggleValue] = useState(state.settings.offline_mode);
   const [analyticsToggleValue, setAnalyticsToggleValue] = useState(state.settings.analytics_opt_in);
   const [lang, setLang] = useState(state.settings.lang || 'eng');
-  const [initialSettingsState] = useState(state.settings);
-  const [locationRangeAnalytics, setLocationRangeAnalytics] = useState(true);
   const onlineCheck = state.isOnline && !state.settings.offline_mode;
-  const geolocationKnown = localStorageKeyExists(constants.CURRENT_LOCATION_KEY);
-  const latitude = state.currentLocation ? state.currentLocation.lat : 49.2827;
-  const longitude = state.currentLocation ? state.currentLocation.long : -123.2;
   /**
    * @summary Handles the change of the Location Range slider
    * @param value is the location range value of the slider
@@ -58,10 +48,6 @@ export default function Settings() {
     setLocationRangeValue(value);
     setSettings({ locationRange: value });
     updateSettings();
-    if (locationRangeAnalytics) {
-      handleSettingAnalytics();
-    }
-    setLocationRangeAnalytics(false);
   };
 
   /**
@@ -74,7 +60,6 @@ export default function Settings() {
     setOfflineToggleValue(value);
     setSettings({ offlineMode: value });
     updateSettings();
-    handleSettingAnalytics();
   };
 
   /**
@@ -99,7 +84,6 @@ export default function Settings() {
     setLang(e.target.value);
     setSettings({ language: e.target.value });
     updateSettings();
-    handleSettingAnalytics();
   };
 
   /**
@@ -108,70 +92,6 @@ export default function Settings() {
    */
   const handleRefresh = () => {
     setAppData(onlineCheck);
-  };
-
-  /**
-   * @summary Creates new analytic objects and send them depending on which ones were changed
-   * @author  Dallas Richmond
-   */
-  const handleSettingAnalytics = () => {
-    if (state.settings.analytics_opt_in && geolocationKnown) {
-      const changedSettings = [] as Analytic[];
-      if (initialSettingsState.lang !== state.settings.lang) {
-        const analytics = {
-          latitude,
-          longitude,
-          usage: {
-            function: 'settings',
-            settings: {
-              valueStr: state.settings.lang,
-              settingType: 'language',
-            },
-          },
-        };
-        changedSettings.push(analytics);
-      }
-      if (initialSettingsState.location_range !== state.settings.location_range) {
-        const analytics = {
-          latitude,
-          longitude,
-          usage: {
-            function: 'settings',
-            settings: {
-              valueStr: state.settings.location_range,
-              settingType: 'location range',
-            },
-          },
-        };
-        changedSettings.push(analytics);
-      }
-      if (initialSettingsState.offline_mode !== state.settings.offline_mode) {
-        const analytics = {
-          latitude,
-          longitude,
-          usage: {
-            function: 'settings',
-            settings: {
-              valueStr: state.settings.offline_mode,
-              settingType: 'offline mode',
-            },
-          },
-        };
-        changedSettings.push(analytics);
-      }
-      if (state.settings.offline_mode) {
-        changedSettings.forEach((analyticData: Analytic) => {
-          setAnalytics(false, analyticData);
-        });
-      } else {
-        OnlineCheck()
-          .then((Online) => {
-            changedSettings.forEach((analyticData: Analytic) => {
-              setAnalytics(Online, analyticData);
-            });
-          });
-      }
-    }
   };
 
   return (
